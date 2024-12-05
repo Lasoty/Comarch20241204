ed8a87f3330ad91840321ade2dd1b1d221771172
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-12-05 11:21:03 +0100

    Testowanie dat i stringów za pomocą FluentAssertion

---
+++ ComarchCwiczenia/Tests/ComarchCwiczenia.Unit.Test/Services/DateCalculatorFluentTests.cs
@@ -0,0 +1,72 @@
+﻿using ComarchCwiczenia.Services;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+
+namespace ComarchCwiczenia.Unit.Test.Services;
+
+[TestFixture]
+public class DateCalculatorFluentTests
+{
+    private DateCalculator cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new DateCalculator();
+    }
+
+    [Test]
+    public void GetNextBusinessDayShouldReturnCorrectValue()
+    {
+        // Arrange
+        DateTime day = 2.December(2024);
+        DateTime expected = 3.December(2024);
+
+        // Act
+        DateTime actual = cut.GetNextBusinessDay(day);
+
+        // Assert
+        actual.Should().Be(expected, "3rd December is after 2nd December.");
+    }
+
+    [Test]
+    public void GetNextBusinessDayShouldReturnCorrectValue2()
+    {
+        // Act
+        cut.GetNextBusinessDay(2.December(2024)).Should().Be(3.December(2024));
+        cut.GetNextBusinessDay(29.November(2024)).Should().Be(2.December(2024));
+        cut.GetNextBusinessDay(30.November(2024)).Should().Be(2.December(2024));
+        cut.GetNextBusinessDay(1.December(2024)).Should().Be(2.December(2024));
+
+        // test na część czasu w datetime
+        cut.GetNextBusinessDay(2.December(2024).At(15,30)).Should().Be(3.December(2024).At(15,30));
+    }
+
+    [Test]
+    public void GetNextBusinessDayShouldBeClosedToInputDate()
+    {
+        DateTime date = 29.November(2024);
+        cut.GetNextBusinessDay(date).Should().BeCloseTo(date, TimeSpan.FromDays(3));
+    }
+
+    public void GetNextBusinessDayShouldBeOnWeekDay()
+    {
+        DateTime date = 29.November(2024);
+
+        DateTime nextDay = cut.GetNextBusinessDay(date);
+        nextDay.DayOfWeek.Should().NotBe(DayOfWeek.Saturday).And.NotBe(DayOfWeek.Sunday);
+    }
+
+    [Test]
+    public void GetNextBusinessDayShouldReturnOneOfExpectedDays()
+    {
+        var expectedDays = new List<DateTime>
+        {
+            29.October(2024),
+            30.October(2024)
+        };
+
+        cut.GetNextBusinessDay(28.October(2024)).Should().BeOneOf(expectedDays);
+    }
+
+}
---
+++ ComarchCwiczenia/Tests/ComarchCwiczenia.Unit.Test/Services/InvoiceServiceFluentTests.cs
@@ -0,0 +1,76 @@
+﻿using ComarchCwiczenia.Services;
+using FluentAssertions;
+
+namespace ComarchCwiczenia.Unit.Test.Services;
+
+[TestFixture]
+public class InvoiceServiceFluentTests
+{
+    private InvoiceService cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new InvoiceService();
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldStartWithINV()
+    {
+        // Act
+        string invoiceNumber = cut.GenerateInvoiceNumber();
+
+        // Assert
+        invoiceNumber.Should().StartWith("INV-");
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldEndWithNumericSuffix()
+    {
+        // Act
+        string invoiceNumber = cut.GenerateInvoiceNumber();
+
+        // Assert
+        invoiceNumber.Should().MatchRegex(@"INV-\d{8}-\d{3}$");
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldContainCurrentDate()
+    {
+        // Arrange
+        string currentDate = DateTime.Now.ToString("yyyyMMdd");
+
+        // Act
+        string invoiceNumber = cut.GenerateInvoiceNumber();
+
+        // Assert
+        invoiceNumber.Should().Contain(currentDate);
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldHaveLengthOf16()
+    {
+        // Act
+        string invoiceNumber = cut.GenerateInvoiceNumber();
+
+        // Assert
+        invoiceNumber.Should().HaveLength(16);
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldNotBeEmpty()
+    {
+        // Act
+        var invoiceNumber = cut.GenerateInvoiceNumber();
+
+        // Assert
+        invoiceNumber.Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public void GenerateInvoiceNumberShouldMachExpectedFormat()
+    {
+        var invoiceNumber = cut.GenerateInvoiceNumber();
+        invoiceNumber.Should().Match("INV-????????-???");
+    }
+}
